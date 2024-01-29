3c2385b1b431a9581880edbbc3d76e1ce9206c91
fastener <mah52x163@163.com>
2024-01-29 16:21:02 +0800

    os

---
+++ README.md
@@ -0,0 +1,18 @@
+请确保已经安装`git`  和 ``bitcoin-core``
+
+###bitcoin-core下载地址:
+https://bitcoincore.org/
+
+安装启动教程自行搜索
+
+随后执行上述命令```atomicals-electrumx.sh``` 自动安装python和依赖项
+
+
+
+###atomicals-electrumx ```.env``` 配置文件说明
+######DAEMON_URL: BTC节点地址
+######DB_DIRECTORY: atomicals节点数据存储路径
+其余默认即可。 详见官方文档。
+
+```.env```中配置完bitcoin节点地址后，运行 ```run.sh``` 即可
+nohup ./run.sh >output.log 2>&1 & 
--- atomicals-electrumx.sh
+++ atomicals-electrumx.sh
@@ -8,11 +8,28 @@ git fetch --all
 
 git checkout 1.3.8.1
 
+os_name=$(uname -s)
+if [ "$os_name" = "Linux" ]; then
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        os_name=$ID
+    fi
+fi
+
+if [ "$os_name" = "ubuntu" ]; then
+    pkg_manager="apt-get"
+elif [ "$os_name" = "centos" ]; then
+    pkg_manager="yum"
+else
+    echo "Not Supported: $os_name"
+    exit 1
+fi
+
 if ! command -v python3 &> /dev/null
 then
     echo "Python3 installing..."
-    sudo apt-get update
-    sudo apt-get install python3 -y
+    sudo $pkg_manager update
+    sudo $pkg_manager install python3 -y
 else
     echo "Python3 installed."
 fi
@@ -20,7 +37,7 @@ fi
 if ! command -v pip3 &> /dev/null
 then
     echo "pip3 installing..."
-    sudo apt-get install python3-pip -y
+    sudo $pkg_manager install python3-pip -y
 else
     echo "pip3 installed."
 fi
